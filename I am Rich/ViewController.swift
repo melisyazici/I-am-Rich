1f99d63a01e7cbda3504333cc4afb613a8f70be9
Melis Yazıcı <melis.yazici95@icloud.com>
2022-01-31 18:17:10 +0100

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Melis Yazıcı on 31.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
